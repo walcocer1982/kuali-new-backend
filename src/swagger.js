a204fbfc1f279d6dacb3e1f806491841312c9951
walcocer1982 <walcocer.1982@gmail.com>
2025-05-09 01:17:42 -0500

    Descripción de los cambios

--- src/swagger.js
+++ src/swagger.js
@@ -2,7 +2,7 @@ const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-const swaggerDocument = yaml.load(fs.readFileSync('c:/Users/LEGION/projects/dev/waltheralcocer/kuali/Kuali-api/swagger.yaml', 'utf8'));
+const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
 
 module.exports = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
